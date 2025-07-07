973bc7e607f47a724d67b6ae708bf3b7665d3535
micaaprocofio <micaelaprocofio@gmail.com>
2025-07-06 23:44:36 -0300

    button for president login

--- src/components/LockedScreen.jsx
+++ src/components/LockedScreen.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Typography, Button } from '@mui/material';
 import { Person } from '@mui/icons-material';
-import AppHeader from './AppHeader'; 
+import AppHeader from './AppHeader'; // Importa el encabezado
 
 const LockedScreen = ({ onAdminAccess }) => {
     return (
@@ -39,6 +39,9 @@ const LockedScreen = ({ onAdminAccess }) => {
                 </Typography>
                 <Typography variant="h6" color="text.secondary">
                     Esperando al próximo elector.
+                </Typography>
+                <Typography variant="body2" color="text.disabled" sx={{ mt: 4 }}>
+                    El presidente de mesa debe habilitar el sistema.
                 </Typography>
             </Box>
         </Box>
